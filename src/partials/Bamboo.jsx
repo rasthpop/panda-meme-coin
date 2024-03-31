6d8eccd4ca58ac3314a3086bce6e54b0af2a3946
rasthpop <rasthetrollman@example.com>
2024-03-31 17:25:05 +0300

    testbranch

--- src/partials/Bamboo.jsx
+++ src/partials/Bamboo.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Bamboo(){
     return(
         <div className='flex flex-row justify-between  fixed top-0 left-0 h-[100vh] w-[100vw]'>
-        <div className="absolute bottom-0 w-1/6"> <img src={'../public/bamboo1.png'} /> </div>
-        <div className="absolute bottom-0 right-0 w-1/6" > <img  src={'../public/bamboo2.png'} /> </div>
+        <div className="absolute bottom-0 w-1/6"> <img src={'/bamboo1.png'} /> </div>
+        <div className="absolute bottom-0 right-0 w-1/6" > <img  src={'/bamboo2.png'} /> </div>
         </div>           
     );
 }
